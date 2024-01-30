38b4d7125529c3c9fcba361392f14b68d7818848
Abduhalim05 <abduhalimabdulkhamidov@gmail.com>
2024-01-30 20:34:47 +0500

    created PokemonController

---
+++ app/Http/Controllers/Api/PokemonController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Pokemon;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+    //
+}
